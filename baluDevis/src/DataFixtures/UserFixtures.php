bfe6a70c0c22961a3af946f87b81f4a25cc0067e
bouaki arthur <204bouaki.athur@gmail.com>
2023-12-11 20:16:41 +0100

    develop fixtures user/ ex messagerie

---
+++ baluDevis/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr-Fr'); // Fix the namespace here
+        $password = 'azerty';
+        $date = new \DateTimeImmutable();
+        $object = (new User())
+            ->setEmail($faker->email())
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($password)
+            ->setCreatedAt($date);
+        $manager->persist($object);
+        $this->addReference('user', $object);
+
+        $object = (new User())
+            ->setEmail($faker->email())
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($password)
+            ->setCreatedAt($date);
+        $manager->persist($object);
+        $this->addReference('admin', $object);
+
+        for ($i = 0; $i < 10; $i++) {
+            $object = (new User())
+                ->setEmail($faker->email())
+                ->setRoles(['ROLE_USER'])
+                ->setPassword($password)
+                ->setCreatedAt($date);
+            $manager->persist($object);
+        }
+
+        $manager->flush();
+    }
+}
