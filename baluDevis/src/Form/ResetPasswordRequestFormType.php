98ccf575fbef0774b7142e101e9f8d39644b9d0e
bouaki arthur <204bouaki.athur@gmail.com>
2024-02-05 14:07:28 +0100

    mep password forget

---
+++ baluDevis/src/Form/ResetPasswordRequestFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ResetPasswordRequestFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+             ->add('email', EmailType::class, [
+                "required" => true,
+                "label" => "Email",
+                'attr' => ["id" => "email", "placeholder" => "votremail@example.com"],
+                "label_attr" => ["class" => "font-bold text-primary"],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
