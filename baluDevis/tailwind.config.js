0e6226e841ae17001c6adf1205cc524053b9b081
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-02-08 12:27:13 +0100

    Added Home Page Integration

--- baluDevis/tailwind.config.js
+++ baluDevis/tailwind.config.js
@@ -4,13 +4,35 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundColor: {
-				secondary: "#2A314B"
+				secondary: "#2A314B",
+                background: "#F8FAFF",
 			},
 			colors: {
-				primary: "#487BD1",
-				secondary: "#2A314B"
-				
-			}
+                primary: "#487BD1",
+                secondary: "#2A314B",
+                delete: "rgba(205,32,32,0.8)",
+                background: "#F8FAFF",
+                gray: "#AAAAAA"
+			},
+			width: {
+				card: "20.5rem",
+				sidebar: "300px",
+			},
+			height: {
+				card: "16.375rem",
+			},
+			borderColor: {
+				primaryLight: "rgba(72,123,209, 0.1)",
+			},
+			boxShadow: {
+                small: "rgba(42,49,75,0.2) 0px 2px 4px 0px"
+            },
+            borderRadius: {
+                "2xl": "4rem"
+            },
+            padding: {
+                side: "300px"
+            }
 		}
 	},
 	plugins: [require("@tailwindcss/forms")]
