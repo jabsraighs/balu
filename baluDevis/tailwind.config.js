a662a81a4daeddfb04ddfd7eadd0d7d2b29e12db
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-02-10 20:58:06 +0100

    Added redirections + fixed template files

--- baluDevis/tailwind.config.js
+++ baluDevis/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
 	content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
 	theme: {
 		screens: {
+			"sm": "640px",
+			"md": "768px",
 			"2md": "960px",
+			"lg": "1024px",
+			"xl": "1280px",
+			"2xl": "1536px",
 		},
 		extend: {
 			backgroundColor: {
