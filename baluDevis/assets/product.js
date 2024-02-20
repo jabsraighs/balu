59dd94244541fdae6eda497c87a83e0c2c54cc65
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-02-21 00:16:34 +0100

    Added search function for products

---
+++ baluDevis/assets/product.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const search = document.querySelector('#table-search')
+    
+    search.addEventListener('input', function() {
+        const searchTerm = this.value.trim().toLowerCase();
+        const rows = document.querySelectorAll('.product-row');
+    
+        rows.forEach(row => {
+            const name = row.querySelector('th').textContent.trim().toLowerCase();
+            if (name.includes(searchTerm)) {
+                row.style.display = '';
+                row.classList.remove('opacity-0');
+            } else {
+                row.classList.add('opacity-0');
+                setTimeout(() => {
+                    row.style.display = 'none';
+                }, 100);
+            }
+        });
+    });
+    
+})
