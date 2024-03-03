c002edc1b79f09f3a86b631aa60e74c45fdf69fb
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-03-03 16:32:43 +0100

    Update MAILER_DSN value in .env file

---
+++ migrations/Version20240303153228.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240303153228 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE cron_job_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE cron_report_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE category (id UUID NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN category.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE client (id UUID NOT NULL, user_client_id UUID NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_C7440455190BE4C5 ON client (user_client_id)');
+        $this->addSql('COMMENT ON COLUMN client.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN client.user_client_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE cron_job (id INT NOT NULL, name VARCHAR(191) NOT NULL, command VARCHAR(1024) NOT NULL, schedule VARCHAR(191) NOT NULL, description VARCHAR(191) NOT NULL, enabled BOOLEAN NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX un_name ON cron_job (name)');
+        $this->addSql('CREATE TABLE cron_report (id INT NOT NULL, job_id INT DEFAULT NULL, run_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, run_time DOUBLE PRECISION NOT NULL, exit_code INT NOT NULL, output TEXT NOT NULL, error TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B6C6A7F5BE04EA9 ON cron_report (job_id)');
+        $this->addSql('CREATE TABLE invoice (id UUID NOT NULL, quote_id UUID DEFAULT NULL, user_invoice_id UUID NOT NULL, client_id UUID NOT NULL, created_at DATE NOT NULL, due_date DATE NOT NULL, payment_status VARCHAR(50) NOT NULL, total_amount DOUBLE PRECISION NOT NULL, name VARCHAR(255) NOT NULL, tva DOUBLE PRECISION NOT NULL, total_tva DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_90651744DB805178 ON invoice (quote_id)');
+        $this->addSql('CREATE INDEX IDX_906517448EEE3711 ON invoice (user_invoice_id)');
+        $this->addSql('CREATE INDEX IDX_9065174419EB6921 ON invoice (client_id)');
+        $this->addSql('COMMENT ON COLUMN invoice.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN invoice.quote_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN invoice.user_invoice_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN invoice.client_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN invoice.created_at IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN invoice.due_date IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('CREATE TABLE payment (id UUID NOT NULL, invoice_id UUID NOT NULL, amount_paid DOUBLE PRECISION NOT NULL, payment_date DATE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_6D28840D2989F1FD ON payment (invoice_id)');
+        $this->addSql('COMMENT ON COLUMN payment.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN payment.invoice_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN payment.payment_date IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('CREATE TABLE product (id UUID NOT NULL, category_id UUID NOT NULL, user_id UUID NOT NULL, name VARCHAR(255) NOT NULL, description TEXT NOT NULL, price DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)');
+        $this->addSql('CREATE INDEX IDX_D34A04ADA76ED395 ON product (user_id)');
+        $this->addSql('COMMENT ON COLUMN product.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN product.category_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN product.user_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE quote (id UUID NOT NULL, client_id UUID DEFAULT NULL, user_quote_id UUID NOT NULL, created_at DATE DEFAULT NULL, expiry_at DATE DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, total_amount DOUBLE PRECISION NOT NULL, tva DOUBLE PRECISION NOT NULL, total_tva DOUBLE PRECISION NOT NULL, description VARCHAR(50) NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_6B71CBF419EB6921 ON quote (client_id)');
+        $this->addSql('CREATE INDEX IDX_6B71CBF43B0DB578 ON quote (user_quote_id)');
+        $this->addSql('COMMENT ON COLUMN quote.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN quote.client_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN quote.user_quote_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN quote.created_at IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN quote.expiry_at IS \'(DC2Type:date_immutable)\'');
+        $this->addSql('CREATE TABLE quote_line (id UUID NOT NULL, quote_id UUID DEFAULT NULL, invoice_id UUID DEFAULT NULL, quantity INT NOT NULL, unit_price DOUBLE PRECISION NOT NULL, subtotal DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_43F3EB7CDB805178 ON quote_line (quote_id)');
+        $this->addSql('CREATE INDEX IDX_43F3EB7C2989F1FD ON quote_line (invoice_id)');
+        $this->addSql('COMMENT ON COLUMN quote_line.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN quote_line.quote_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN quote_line.invoice_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE "user" (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.available_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('COMMENT ON COLUMN messenger_messages.delivered_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE client ADD CONSTRAINT FK_C7440455190BE4C5 FOREIGN KEY (user_client_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE cron_report ADD CONSTRAINT FK_B6C6A7F5BE04EA9 FOREIGN KEY (job_id) REFERENCES cron_job (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_90651744DB805178 FOREIGN KEY (quote_id) REFERENCES quote (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_906517448EEE3711 FOREIGN KEY (user_invoice_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_9065174419EB6921 FOREIGN KEY (client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE payment ADD CONSTRAINT FK_6D28840D2989F1FD FOREIGN KEY (invoice_id) REFERENCES invoice (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADA76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote ADD CONSTRAINT FK_6B71CBF419EB6921 FOREIGN KEY (client_id) REFERENCES client (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote ADD CONSTRAINT FK_6B71CBF43B0DB578 FOREIGN KEY (user_quote_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote_line ADD CONSTRAINT FK_43F3EB7CDB805178 FOREIGN KEY (quote_id) REFERENCES quote (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE quote_line ADD CONSTRAINT FK_43F3EB7C2989F1FD FOREIGN KEY (invoice_id) REFERENCES invoice (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE cron_job_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE cron_report_id_seq CASCADE');
+        $this->addSql('ALTER TABLE client DROP CONSTRAINT FK_C7440455190BE4C5');
+        $this->addSql('ALTER TABLE cron_report DROP CONSTRAINT FK_B6C6A7F5BE04EA9');
+        $this->addSql('ALTER TABLE invoice DROP CONSTRAINT FK_90651744DB805178');
+        $this->addSql('ALTER TABLE invoice DROP CONSTRAINT FK_906517448EEE3711');
+        $this->addSql('ALTER TABLE invoice DROP CONSTRAINT FK_9065174419EB6921');
+        $this->addSql('ALTER TABLE payment DROP CONSTRAINT FK_6D28840D2989F1FD');
+        $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04AD12469DE2');
+        $this->addSql('ALTER TABLE product DROP CONSTRAINT FK_D34A04ADA76ED395');
+        $this->addSql('ALTER TABLE quote DROP CONSTRAINT FK_6B71CBF419EB6921');
+        $this->addSql('ALTER TABLE quote DROP CONSTRAINT FK_6B71CBF43B0DB578');
+        $this->addSql('ALTER TABLE quote_line DROP CONSTRAINT FK_43F3EB7CDB805178');
+        $this->addSql('ALTER TABLE quote_line DROP CONSTRAINT FK_43F3EB7C2989F1FD');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE cron_job');
+        $this->addSql('DROP TABLE cron_report');
+        $this->addSql('DROP TABLE invoice');
+        $this->addSql('DROP TABLE payment');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE quote');
+        $this->addSql('DROP TABLE quote_line');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
