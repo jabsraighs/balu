9dff639445b2c7d93e9cd61777f709ab0581ad25
Ulysseassoo <ulysse.asso-o-emane@supinternet.fr>
2024-03-03 17:29:26 +0100

    Update database configuration

---
+++ export-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Ensure the file is empty.
+cat '' > config_db/.env
+
+# Map the database information from the PLATFORM_RELATIONSHIPS variable into the YAML file.
+# Use this process to use whatever variable names your app needs.
+
+printf "DATABASE_URL: %s\n" $(echo $PLATFORM_VARIABLES | base64 --decode | jq -r ".DATABASE_URL") >> config_db/.env
