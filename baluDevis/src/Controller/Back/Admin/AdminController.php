f61b64e09b485419dbaf9db4599abba5c4649564
bouaki arthur <204bouaki.athur@gmail.com>
2024-03-10 00:53:29 +0100

    modif index admin et controller

--- baluDevis/src/Controller/Back/Admin/AdminController.php
+++ baluDevis/src/Controller/Back/Admin/AdminController.php
@@ -3,20 +3,27 @@
 namespace App\Controller\Back\Admin;
 
 use App\Entity\User;
+use App\Repository\ClientRepository;
+use App\Repository\InvoiceRepository;
+use App\Repository\QuoteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 #[isGranted("ROLE_ADMIN")]
 class AdminController extends AbstractController
 {
     #[Route('/', name: '')]
-    public function index(User $user): Response {
+    public function index(User $user,InvoiceRepository $invoiceRepository,QuoteRepository $quoteRepository, ClientRepository $clientRepository): Response {
         if($this->isGranted('ROLE_ADMIN')) {
-            return $this->render('Back/admin/index.html.twig', [
-                'controller_name' => 'AdminController',
-            ]);
+            $invoices = $invoiceRepository->findAll();
+            $quotes= $quoteRepository->findAll();
+            $clients = $clientRepository->findAll();
+        return $this->render('Back/admin/index.html.twig', [
+            'quotes' => $quotes,
+            'invoices' => $invoices,
+            'clients' => $clients
+        ]);
         }
     }
 }
