fd0e04d5a5aabd360b4e7dfb8f2ea38f7e58d2c5
jabsraughs <204bouaki.arthur@gmail.com>
2024-04-06 18:54:54 +0200

    template front entreprise

--- baluDevis/src/Form/ProductType.php
+++ baluDevis/src/Form/ProductType.php
@@ -7,6 +7,7 @@ use App\Entity\Product;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,9 +16,9 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
+            ->add('name',TextType::class, [])
+            ->add('description',TextType::class, [])
+            ->add('price',IntergerType::class,[])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
